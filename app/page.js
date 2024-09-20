28110e7d190df6eb993b813008e6225aac9cf531
Alexander Viafara <mrearsbig@gmail.com>
2024-09-20 13:07:32 -0500

    Server and Client Components

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import LikeButton from './like-button';
 
 function Header({ title }) {
     console.log(title); // "React"
@@ -8,13 +8,6 @@ function Header({ title }) {
 export default function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
 
-    const [likes, setLikes] = useState(0);
-
-    function handleClick() {
-        console.log("increment like count");
-        setLikes(likes + 1);
-    }
-
     return (
         <div>
             <Header title="Develop. Preview. Ship." />
@@ -23,7 +16,7 @@ export default function HomePage() {
                     <li key={name}>{name}</li>
                 ))}
             </ul>
-            <button onClick={handleClick}>Like ({likes})</button>
+            <LikeButton />
         </div>
     );
 }
