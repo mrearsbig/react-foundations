21c01c2485fafaf4bbee2ae547cb8191b63f2223
Alexander Viafara <mrearsbig@gmail.com>
2024-09-20 12:44:08 -0500

    Installing Next.js

---
+++ index.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function Header({ title }) {
+    console.log(title); // "React"
+    return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    const [likes, setLikes] = useState(0);
+
+    function handleClick() {
+        console.log("increment like count");
+        setLikes(likes + 1);
+    }
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship." />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+            <button onClick={handleClick}>Like ({likes})</button>
+        </div>
+    );
+}
